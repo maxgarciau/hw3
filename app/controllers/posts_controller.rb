02072fea6ef28dbb2994c5182c05ceaffe7c687c
maxgarciau <maximiliano.garcia@kellogg.northwestern.edu>
2022-05-16 20:23:07 +0000

    created controllers, new, and index spaces

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        @post = Post.new
+    end
+      
+    def create
+        @post = Place.new
+        @post["title"]=params["post"]["title"]
+        @post["description"]=params["post"]["description"]
+        @post["posted_on"]=params["post"]["posted_on"]
+        @post["place_id"]=params["post"]["place_id"]
+        @post.save
+        redirect_to "/posts"
+    end
+end
