ae39be2386b64ced61381b7479afe1df844791cf
maxgarciau <maximiliano.garcia@kellogg.northwestern.edu>
2022-05-17 03:22:28 +0000

    fixed the post show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
     
     def new
         @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @place["id"]
     end
       
     def create
@@ -14,6 +16,6 @@ class PostsController < ApplicationController
         @post["posted_on"]=params["post"]["posted_on"]
         @post["place_id"]=params["post"]["place_id"]
         @post.save
-        redirect_to "/places/"
+        redirect_to "/places/#{@post["place_id"]}/"
     end
 end
