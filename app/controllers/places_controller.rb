f1f09b618fd37a81e9525286a454c8e6c6873238
maxgarciau <maximiliano.garcia@kellogg.northwestern.edu>
2022-05-18 02:54:09 +0000

    deleted the delete button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,9 +19,9 @@ class PlacesController < ApplicationController
         redirect_to "/places"
     end
 
-    def destroy
-        @place = Place.find_by({ "id" => params["id"] })
-        @place.destroy
-        redirect_to "/places"
-    end
+    # def destroy
+    #     @place = Place.find_by({ "id" => params["id"] })
+    #     @place.destroy
+    #     redirect_to "/places"
+    # end
 end
